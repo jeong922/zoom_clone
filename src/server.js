8952bf4d77374c7c6a1c75551126ec40d3e4046f
jeong922 <hwajeong922@naver.com>
2022-10-19 15:54:55 +0900

    admin panel

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'http';
 import express from 'express';
-// import WebSocket from 'ws';
-import SocketIO from 'socket.io';
+import { Server } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 const app = express();
 
@@ -12,7 +12,16 @@ app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ['https://admin.socket.io'],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRoom() {
   const { sids, rooms } = wsServer.sockets.adapter;
